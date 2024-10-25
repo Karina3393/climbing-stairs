56acc6bb81e4e59decc548eb82ffa0e438407ebd
карина <@DESKTOP-FP9I0KA>
2024-10-25 13:29:16 +0300

    Add project files.

---
+++ climbing-stairs/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace climbing_stairs
+{
+    public class Solution
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n <= 2) return n;
+            int first = 1, second = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                int third = first + second;
+                first = second;
+                second = third;
+            }
+            return second;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+
+            Console.WriteLine(solution.ClimbStairs(1));
+            Console.WriteLine(solution.ClimbStairs(2));
+            Console.WriteLine(solution.ClimbStairs(3));
+            Console.WriteLine(solution.ClimbStairs(4));
+            Console.WriteLine(solution.ClimbStairs(5));
+        }
+    }
+}
